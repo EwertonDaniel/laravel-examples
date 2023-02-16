e75987585630048cff2a6c129326354c8abc0599
Ewerton Daniel <ewertondaniel@icloud.com>
2023-02-16 20:48:46 -0300

    [UPDATE] - Icons and page title base

---
+++ resources/views/components/icons.blade.php
@@ -0,0 +1,5 @@
+<link rel="apple-touch-icon" sizes="180x180" href="https://laravel.com/img/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://laravel.com/img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://laravel.com/img/favicon/favicon-16x16.png">
+<link rel="mask-icon" href="https://laravel.com/img/favicon/safari-pinned-tab.svg" color="#ff2d20">
+<link rel="shortcut icon" href="https://laravel.com/img/favicon/favicon.ico">
